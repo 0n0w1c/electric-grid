9b57e3db6f4582b68253dc765c924b25c5179e48
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-11-11 05:58:05 -0600

    Version 0.0.1

--- data-updates.lua
+++ data-updates.lua
@@ -1,16 +1,23 @@
-data.raw["electric-pole"]["small-electric-pole"].maximum_wire_distance = 8.5
-data.raw["electric-pole"]["small-electric-pole"].supply_area_distance = 3
+-- Modify electric poles
+data.raw["electric-pole"]["small-electric-pole"].maximum_wire_distance =
+    data.raw["electric-pole"]["medium-electric-pole"].maximum_wire_distance
 
-data.raw["electric-pole"]["medium-electric-pole"].maximum_wire_distance = 10
+data.raw["electric-pole"]["small-electric-pole"].supply_area_distance =
+    data.raw["electric-pole"]["medium-electric-pole"].supply_area_distance
+
 data.raw["electric-pole"]["medium-electric-pole"].supply_area_distance = 0
 
-data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance = 18
+data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance =
+    data.raw["electric-pole"]["substation"].maximum_wire_distance
+
 data.raw["electric-pole"]["big-electric-pole"].supply_area_distance = 0
 
+-- Remove the power switch
 data.raw["power-switch"]["power-switch"].hidden = true
 data.raw["item"]["power-switch"].hidden = true
 data.raw["recipe"]["power-switch"].hidden = true
 
+-- Update base technology research effects
 table.insert(data.raw["technology"]["electric-energy-distribution-1"].effects,
     { type = "unlock-recipe", recipe = "eg-huge-electric-pole" })
 
