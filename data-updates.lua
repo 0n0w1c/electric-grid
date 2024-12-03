fe01c20546c171806e594b1e18c3cccbcecfa4fa
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-12-03 08:05:56 -0600

    Version 0.4.4

--- data-updates.lua
+++ data-updates.lua
@@ -1,24 +1,19 @@
-data.raw["electric-pole"]["small-electric-pole"].maximum_wire_distance =
-    data.raw["electric-pole"]["medium-electric-pole"].maximum_wire_distance
-data.raw["electric-pole"]["small-electric-pole"].supply_area_distance =
-    data.raw["electric-pole"]["medium-electric-pole"].supply_area_distance
+local small_pole = data.raw["electric-pole"]["small-electric-pole"]
+local medium_pole = data.raw["electric-pole"]["medium-electric-pole"]
+local big_pole = data.raw["electric-pole"]["big-electric-pole"]
+local substation = data.raw["electric-pole"]["substation"]
 
-if mods["aai-industry"] then
-    data.raw["electric-pole"]["small-iron-electric-pole"].maximum_wire_distance =
-        data.raw["electric-pole"]["medium-electric-pole"].maximum_wire_distance
-    data.raw["electric-pole"]["small-iron-electric-pole"].supply_area_distance =
-        data.raw["electric-pole"]["medium-electric-pole"].supply_area_distance
-end
+small_pole.maximum_wire_distance = medium_pole.maximum_wire_distance
+small_pole.supply_area_distance = medium_pole.supply_area_distance
 
-data.raw["electric-pole"]["medium-electric-pole"].supply_area_distance = 0
-data.raw["electric-pole"]["medium-electric-pole"].light = constants.EG_MEDIUM_POLE_LIGHT
+medium_pole.supply_area_distance = 0
+medium_pole.light = constants.EG_MEDIUM_POLE_LIGHT
 
-data.raw["electric-pole"]["big-electric-pole"].supply_area_distance = 0
-data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance =
-    data.raw["electric-pole"]["substation"].maximum_wire_distance
-data.raw["electric-pole"]["big-electric-pole"].light = constants.EG_BIG_POLE_LIGHT
+big_pole.supply_area_distance = 0
+big_pole.maximum_wire_distance = substation.maximum_wire_distance
+big_pole.light = constants.EG_BIG_POLE_LIGHT
 
-data.raw["electric-pole"]["substation"].next_upgrade = "eg-ugp-substation-displayer"
+substation.next_upgrade = "eg-ugp-substation-displayer"
 
 -- Hide switch
 data.raw["power-switch"]["power-switch"].hidden = true
@@ -30,20 +25,28 @@ if constants.EG_EVEN_ALIGN_RADAR then
     data.raw["radar"]["radar"].collision_box = { { -1.51, -1.51 }, { 1.51, 1.51 } }
 end
 
--- Technology
+-- Technologies
 table.insert(data.raw["technology"]["electric-energy-distribution-1"].effects,
     { type = "unlock-recipe", recipe = "eg-huge-electric-pole" })
 
 table.insert(data.raw["technology"]["electric-energy-distribution-2"].effects,
     { type = "unlock-recipe", recipe = "eg-ugp-substation-displayer" })
 
+-- Mod the mods
+if mods["aai-industry"] then
+    local iron_pole = data.raw["electric-pole"]["small-iron-electric-pole"]
+
+    iron_pole.maximum_wire_distance = medium_pole.maximum_wire_distance
+    iron_pole.supply_area_distance = medium_pole.supply_area_distance
+end
+
 if mods["ConnectionBox"] then
-    data.raw["electric-pole"]["connection-box"].supply_area_distance = 0
-    data.raw["electric-pole"]["connection-box"].maximum_wire_distance =
-        data.raw["electric-pole"]["substation"].maximum_wire_distance / 2
-    data.raw["electric-pole"]["connection-box"].minable.mining_time =
-        data.raw["electric-pole"]["substation"].minable.mining_time
-    data.raw["electric-pole"]["connection-box"].light = constants.EG_MINI_POLE_LIGHT
+    local connection_box = data.raw["electric-pole"]["connection-box"]
+
+    connection_box.supply_area_distance = 0
+    connection_box.maximum_wire_distance = substation.maximum_wire_distance / 2
+    connection_box.minable.mining_time = substation.minable.mining_time
+    connection_box.light = constants.EG_MINI_POLE_LIGHT
 
     data.raw["item"]["connection-box"].icon = constants.EG_GRAPHICS .. "/icons/eg-connection-box.png"
     data.raw["item"]["connection-box"].icon_size = 256
@@ -54,10 +57,15 @@ if mods["ConnectionBox"] then
         { type = "item", name = "copper-cable", amount = 3 }
     }
 
+    if mods["quality"] and data.raw["recipe"]["connection-box-recycling"] then
+        data.raw["recipe"]["connection-box-recycling"].results =
+        {
+            { type = "item", name = "iron-stick",   amount = 0.125, extra_count_fraction = 0.125 },
+            { type = "item", name = "copper-cable", amount = 0.125, extra_count_fraction = 0.125 },
+        }
+    end
+
     -- Lock recipe due to iron stick ingredient
     data.raw["recipe"]["connection-box"].enabled = false
-    table.insert(data.raw["technology"]["circuit-network"].effects, {
-        recipe = "connection-box",
-        type = "unlock-recipe"
-    })
+    table.insert(data.raw["technology"]["circuit-network"].effects, { recipe = "connection-box", type = "unlock-recipe" })
 end
